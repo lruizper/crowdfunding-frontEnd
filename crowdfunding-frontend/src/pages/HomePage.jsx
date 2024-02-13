e411dd2835d5fdddc9314cc1cb7b02be14393cff
lruizper <lruizper@gmail.com>
2024-02-13 20:03:24 +0800

    Remove unused CSS and React components, added dummy data and displayed in homepage

---
+++ crowdfunding-frontend/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import {allProjects} from "../data";
+
+function HomePage() {
+    return (
+        <div>
+        {allProjects.map((projectData, key) => {
+            return <div key={key}>{projectData.title}</div>;
+        })}
+        </div>
+        );
+    }
+    export default HomePage;

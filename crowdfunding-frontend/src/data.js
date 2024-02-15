723edd673c16b0960dd1306e1edccb0315fec045
lruizper <lruizper@gmail.com>
2024-02-15 14:58:47 +0800

    project cards with basic styling

--- crowdfunding-frontend/src/data.js
+++ crowdfunding-frontend/src/data.js
@@ -50,5 +50,12 @@ export const oneProject = {
     description: "The first project.",
     goal: 150,
     image: "https://via.placeholder.com/300.jpg", is_open: false, date_created: "2020-03-20T14:22:23.382748Z", owner: 1,
-    pledges: [{ id: 1, amount: 100, comment: "A comment for the pledge", anonymous: false, supporter: 3, project_id: 1, },]
+    pledges: [{
+        id: 1,
+        amount: 100,
+        comment: "A comment for the pledge",
+        anonymous: false,
+        supporter: 3,
+        project_id: 1,
+    },]
 }
--- crowdfunding-frontend/src/pages/HomePage.jsx
+++ crowdfunding-frontend/src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import {allProjects} from "../data";
+import ProjectCard from "../components/ProjectCard";
+import "./HomePage.css";
 
 function HomePage() {
     return (
-        <div>
+        <div id="project-list">
         {allProjects.map((projectData, key) => {
-            return <div key={key}>{projectData.title}</div>;
+            return <ProjectCard key={key} projectData={projectData} />;
         })}
         </div>
         );

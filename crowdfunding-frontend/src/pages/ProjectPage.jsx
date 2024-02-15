cd40479f04c8082e3ed4dc196e8a8fb93a758d84
lruizper <lruizper@gmail.com>
2024-02-15 15:14:36 +0800

    ProjectPage component to display project details and pledges

--- crowdfunding-frontend/src/pages/ProjectPage.jsx
+++ crowdfunding-frontend/src/pages/ProjectPage.jsx
@@ -1,3 +1,25 @@
+import { oneProject } from "../data";
+
 function ProjectPage() {
-    return<h1>This is the project page.</h1>;}
+    return (
+        <div>
+            <h2>{oneProject.title}</h2>
+            <h3>Created at: {oneProject.date_created}</h3>
+            <img src={oneProject.image} />
+            <h3>Goal: {oneProject.goal}</h3>
+            <h4>{`Status: ${oneProject.is_open}`}</h4>
+            <p>{oneProject.description}</p>
+
+            <h3>Pledges:</h3>
+            <ul>
+                {oneProject.pledges.map((pledge, key) => {
+                    return (
+                        <li key={key}>{pledge.amount} from {pledge.supporter}
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
+    );
+}
 export default ProjectPage

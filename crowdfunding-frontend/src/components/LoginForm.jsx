878e04fbafa6a063ae82c62da7a64542a586b30f
lruizper <lruizper@gmail.com>
2024-02-20 14:08:08 +0800

    Add state and event handler for login form inputs

--- crowdfunding-frontend/src/components/LoginForm.jsx
+++ crowdfunding-frontend/src/components/LoginForm.jsx
@@ -1,15 +1,27 @@
 import "./LoginForm.css";
+import {useState} from "react";
 
 function LogInForm() {
+    const [ credentials, setCredentials ] = useState({username: "", password: ""});
+    const handleChange = (e) => {
+        const { id, value } = e.target;
+        setCredentials((prevCredentials) => ({
+            ...prevCredentials,
+            [id]: value,
+        }));
+    };
+
     return (
         <form className="myForm">
             <div className="myInput">
                 <label htmlFor="username">Username: </label>
-                <input type="text" id="username" placeholder="Your username" />
+                {/* <input type="text" id="username" placeholder="Your username" /> */}
+                <input type="text" id="username" onChange={handleChange} placeholder="Your username" />
             </div>
             <div className="myInput">
                 <label htmlFor="password">Password: </label>
-                <input type="password" id="password" placeholder="Your password" />
+                {/* <input type="password" id="password" placeholder="Your password" /> */}
+                <input type="password" id="password" onChange={handleChange} placeholder="Your password" />
             </div>
             <button type="submit">Log In</button>
         </form>

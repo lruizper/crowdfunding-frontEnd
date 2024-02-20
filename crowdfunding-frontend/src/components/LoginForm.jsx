a1137c6959955edbda609df35ccdbda950329386
lruizper <lruizper@gmail.com>
2024-02-20 14:57:49 +0800

    completed step 3 of notes, token visible on console when exisiting user attempts login

--- crowdfunding-frontend/src/components/LoginForm.jsx
+++ crowdfunding-frontend/src/components/LoginForm.jsx
@@ -1,8 +1,9 @@
 import "./LoginForm.css";
-import {useState} from "react";
+import { useState } from "react";
+import postLogin from "../api/post-login";
 
 function LogInForm() {
-    const [ credentials, setCredentials ] = useState({username: "", password: ""});
+    const [credentials, setCredentials] = useState({ username: "", password: "" });
     const handleChange = (e) => {
         const { id, value } = e.target;
         setCredentials((prevCredentials) => ({
@@ -10,20 +11,31 @@ function LogInForm() {
             [id]: value,
         }));
     };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        // if (!credentials.username || !credentials.password) {
+        //     alert("Please enter both a username and password");
+        //     return;
+        // }
+        if (credentials.username && credentials.password) {
+            postLogin(credentials.username, credentials.password)
+                .then((response) => {
+                    console.log(response);
+                });
+        }
+    };
 
     return (
         <form className="myForm">
             <div className="myInput">
                 <label htmlFor="username">Username: </label>
-                {/* <input type="text" id="username" placeholder="Your username" /> */}
                 <input type="text" id="username" onChange={handleChange} placeholder="Your username" />
             </div>
             <div className="myInput">
                 <label htmlFor="password">Password: </label>
-                {/* <input type="password" id="password" placeholder="Your password" /> */}
                 <input type="password" id="password" onChange={handleChange} placeholder="Your password" />
             </div>
-            <button type="submit">Log In</button>
+            <button type="submit" onClick={handleSubmit}>Log In</button>
         </form>
     )
 }
